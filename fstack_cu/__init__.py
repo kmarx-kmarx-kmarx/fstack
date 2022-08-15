df23dfd934abb09fe7be63b0dff80338502953ef
Kevin Marx <kevinmarx14@gmail.com>
2022-08-15 11:40:16 -0700

    bug fixes

--- fstack_cu/__init__.py
+++ fstack_cu/__init__.py
@@ -1,14 +1,13 @@
 import numpy as np
 import cv2
-import scipy.ndimage
 import time
 import cupy as cp
 import cupyx.scipy.ndimage
 
-def fstack_cu(paths, focus, WSize=9, alpha=0.2, sth=13):
+def fstack_cu_images(imgs, focus, WSize=9, alpha=0.2, sth=13):
     t0 = time.time()
     print("FMeasure", end = ": ")
-    imgs = np.array([cv2.imread(path) for path in paths], dtype='f')
+    imgs = np.array(imgs, dtype='f')
     isColor, stack, h, w, c = parseInputs(imgs)
     
     # If isColor, store the color version and overwrite imgs with black and white
@@ -29,8 +28,10 @@ def fstack_cu(paths, focus, WSize=9, alpha=0.2, sth=13):
     print("gauss3P: " + str(t2 - t1))
     
     # Estimate RMS error along slice axis
-    err = np.array([np.abs(focus_measure[i,:,:] - A * np.exp(-(1+focus[i]-u)**2/(2*(s**2)))) for i in range(len(focus))])
-    err[np.isnan(err)] = np.max(err[~np.isnan(err)])
+    fm = cp.array(focus_measure)
+    err = cp.array([cp.abs(fm[i,:,:] - A * cp.exp(-(1+focus[i]-u)**2/(2*(s**2)))) for i in range(len(focus))])
+    err[cp.isnan(err)] = cp.max(err[~cp.isnan(err)])
+    err = err.get()
     # Sum along slice axis
     err = np.sum(err, axis=0)
     err = err/(FMax * stack)
@@ -72,6 +73,75 @@ def fstack_cu(paths, focus, WSize=9, alpha=0.2, sth=13):
     print("fusion: " + str(t5 - t4))
     print("total time: " + str(t5-t0))
     return retval
+
+
+# def fstack_cu(paths, focus, WSize=9, alpha=0.2, sth=13):
+#     t0 = time.time()
+#     print("FMeasure", end = ": ")
+#     imgs = np.array([cv2.imread(path) for path in paths], dtype='f')
+#     isColor, stack, h, w, c = parseInputs(imgs)
+    
+#     # If isColor, store the color version and overwrite imgs with black and white
+#     if(isColor):
+#         imgs_color = np.copy(imgs)
+#         imgs = np.array([cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) for image in imgs], dtype='f')
+#     # Take only one channel
+#     if(c>1):
+#         imgs = imgs[:,:,:,0]
+    
+#     focus_measure = np.array([gfocus(img, WSize) for img in imgs])
+#     t1 = time.time()
+#     print(t1 - t0)
+    
+#     print("SMeasure")
+#     u, s, A, FMax = gauss3P(focus, focus_measure)
+#     t2 = time.time()
+#     print("gauss3P: " + str(t2 - t1))
+    
+#     # Estimate RMS error along slice axis
+#     err = np.array([np.abs(focus_measure[i,:,:] - A * np.exp(-(1+focus[i]-u)**2/(2*(s**2)))) for i in range(len(focus))])
+#     err[np.isnan(err)] = np.max(err[~np.isnan(err)])
+#     # Sum along slice axis
+#     err = np.sum(err, axis=0)
+#     err = err/(FMax * stack)
+#     err = cp.array(err)
+#     t3 = time.time()
+#     print("err: " + str(t3 - t2))
+#     # might need to transpose focus_measure to slice across slices
+#     # renormalize focus_measure
+#     focus_measure = cp.array([fmeas/FMax for fmeas in focus_measure])
+#     # Filter the err
+#     kernel = cp.ones((WSize, WSize))/(WSize * WSize)
+#     inv_psnr = cp.array(cupyx.scipy.ndimage.correlate(err, kernel, mode='nearest'), dtype='f')
+        
+#     S = 20*cp.log10(1.0/inv_psnr)
+#     S[cp.isnan(S)] = cp.min(S[~cp.isnan(S)])
+    
+#     phi = 0.5*(1+cp.tanh(alpha*(S-sth)))/alpha
+#     phi1 = cupyx.scipy.ndimage.median_filter(phi, size=3)
+    
+#     focus_measure = cp.array([(0.5 + 0.5*cp.tanh(phi1*(slc-1))) for slc in focus_measure])
+    
+#     # Sum along slice axis
+#     fmn = cp.sum(focus_measure,axis=0).get()
+#     focus_measure = focus_measure.get()
+    
+#     t4 = time.time()
+#     print("filter: " + str(t4 - t3))    
+    
+#     if(isColor):
+#         imgs_color[:,:,0] = np.sum(imgs_color[:,:,0] * focus_measure , axis=0)/fmn
+#         imgs_color[:,:,1] = np.sum(imgs_color[:,:,1] * focus_measure , axis=0)/fmn
+#         imgs_color[:,:,2] = np.sum(imgs_color[:,:,2] * focus_measure , axis=0)/fmn
+#         retval =  imgs_color
+#     else:
+#         imgs = np.sum(imgs * focus_measure , axis=0)/fmn
+#         retval =  imgs
+        
+#     t5 = time.time()
+#     print("fusion: " + str(t5 - t4))
+#     print("total time: " + str(t5-t0))
+#     return retval
         
 def gfocus(im, WSize):
     # verified - matches matlab output
@@ -124,7 +194,7 @@ def gauss3P(x, Y):
     a = y1 - b*x1 - c*x1**2
     A = cp.exp(a + u**2/(2*s**2))
     
-    return cp.real(u).get(), cp.real(s).get(), cp.real(A).get(), cp.real(Ymax).get()
+    return cp.real(u), cp.real(s), cp.real(A), cp.real(Ymax).get()
 
 
 def parseInputs(image_list):
